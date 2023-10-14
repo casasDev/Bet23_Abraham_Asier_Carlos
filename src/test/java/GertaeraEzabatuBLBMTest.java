9e0cb394660db1a139a9c5d2dc18eaf315884683
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-10-15 00:43:13 +0200

    mas test pa mi metodo

--- src/test/java/GertaeraEzabatuBLBMTest.java
+++ src/test/java/GertaeraEzabatuBLBMTest.java
@@ -9,17 +9,21 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 
+import com.sun.tools.javac.util.List;
+
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 import dataAccess.DataAccess;
 import domain.Event;
 import domain.Question;
+import domain.Quote;
 import exceptions.EventFinished;
 import exceptions.QuestionAlreadyExist;
 
@@ -448,5 +452,66 @@ public class GertaeraEzabatuBLBMTest {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+     }
+     
+     @Test
+     public void test10() {
+    	 
+    	 try {
+    	 
+    	//define paramaters
+			String queryText="proba galdera";
+			Float betMinimum=new Float(2);
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date oneDate=null;;
+			try {
+				oneDate = sdf.parse("05/11/2023");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		//configure Mock
+		Mockito.doReturn(oneDate).when(mockedEvent).getEventDate();
+		Mockito.doReturn(2).when(mockedEvent).getEventNumber();
+		ArrayList<Question> arrayPre =new ArrayList<Question>();
+		Question q =  (dataAccess).createQuestion(mockedEvent,queryText,betMinimum);
+		Question q1 =  (dataAccess).createQuestion(mockedEvent,queryText,betMinimum);
+		
+		q.setResult("2 a 0");
+		q1.setResult("34");
+		arrayPre.add(q);
+		arrayPre.add(q1);
+		Mockito.doReturn(arrayPre).when(mockedEvent).getQuestions();
+    	 
+         // Crear un objeto Event con fecha futura y preguntas con resultados definidos
+         Event event = new Event();
+         event.setEventDate(new Date(System.currentTimeMillis() + 10000));
+         Question question1 = new Question();
+         question1.setResult("Resultado 1");
+         Question question2 = new Question();
+         question2.setResult("Resultado 2");
+         event.addQuestion(queryText,betMinimum);
+         event.addQuestion(queryText,betMinimum);
+         
+         // Mocks para EntityManager y TypedQuery
+         EntityManager entityManager = Mockito.mock(EntityManager.class);
+         TypedQuery<Quote> query = Mockito.mock(TypedQuery.class);
+         Mockito.doReturn(event).when(entityManager.find(Event.class, event));
+         Mockito.doReturn(query).when(entityManager.createQuery("yepa", (Quote.class)));
+         Mockito.doReturn(query).when(query.setParameter(1, event.getEventNumber()));
+         Mockito.doReturn(List.of(new Quote())).when(query.getResultList());
+      
+         
+         // Llamar al método y verificar el resultado
+         assertTrue(sut.gertaeraEzabatu(new Event()));
+         
+     } catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			assertTrue(true);
+			} catch (QuestionAlreadyExist e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
      }
 }
